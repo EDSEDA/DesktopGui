345266d75507415a2b22222f60c936474bee2402
niksul <nikita.sulimenko@smddev.com>
2024-02-16 01:09:52 +0300

    clear code

--- main.py
+++ main.py
@@ -5,18 +5,6 @@ from PyQt5.QtWidgets import QApplication
 from gui.mainwindow import MainWindow
 
 
-def console_listener(window):
-    while True:
-        user_input = input("Введите '1' для обновления профиля: ")
-        if user_input == '1':
-            # Обновите это соответствующими данными
-            new_id = '123456789'
-            new_info = 'Новая информация\nНовые данные'
-            new_avatar_path = 'resources/test_img/test_img.jpg'
-            # Используйте методы Qt для обновления GUI из основного потока
-            window.update_client_profile(new_id, new_info, new_avatar_path)
-
-
 def start_QApp():
     app = QApplication(sys.argv)
     main_window = MainWindow()
